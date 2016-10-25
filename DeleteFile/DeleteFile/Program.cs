099c70bb986b467f1de12a657f9b1c09e59221f1
Makoto Taniguchi <world.for.ash@gmail.com>
2016-10-25 20:15:51 +0900

    Add files via upload
    WindowsでSourceTreeを使用する際に、
    カスタムコマンドで使用するExeを作成

---
+++ DeleteFile/DeleteFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootPath = args[0];
+
+            var filePaths = args.Skip(1).ToList();
+            foreach (var filePath in filePaths)
+            {
+                var deleFilePath = string.Format(@"{0}\{1}", rootPath, filePath.Replace(@"/", @"\"));
+                System.IO.File.Delete(deleFilePath);
+            }
+        }
+    }
+}
